a4d4351f61a2835583e51ae1a4fec560c5e78a0a
omszag <53254676+omszag@users.noreply.github.com>
2019-09-30 18:25:30 +0200

    Create Hangar.java

---
+++ Hangar.java
@@ -0,0 +1,12 @@
+public class Hangar {
+
+    public static void main(String[] args) {
+
+	Car clio = new Car("Clio",0);
+		System.out.println(clio.doStuff());
+
+	Boat titanic = new Boat("Titanic",0);
+		System.out.println(titanic.doStuff());
+
+    }
+}
